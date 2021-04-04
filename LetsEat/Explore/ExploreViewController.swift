1a75319aa31b61edb20de21b8ddab5b797d37171
Chingis <cbleadr@gmail.com>
2021-04-04 18:57:28 +0300

    chapter 10

---
+++ LetsEat/Explore/ExploreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by Чингис Бахтевалеев on 01.04.2021.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+        return headerView
+    }
+    
+    //how many cells to display
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20 // no need in return if only one line code
+    }
+    
+    //what to display in each collection view cell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        return cell
+    }
+    
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
